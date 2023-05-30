a750b414fd403cdc93145d27c23b010f3a998738
Junyi Zhang <junyizhang@sjtu.edu.cn>
2023-05-30 09:59:14 +0800

    minor update

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ def get_logger(output_file):
     log_format = "[<green>{time:YYYY-MM-DD HH:mm:ss}</green>] {message}"
     logger.configure(handlers=[{"sink": sys.stderr, "format": log_format}])
     while os.path.exists(output_file):
-        output_file = output_file.replace('.log', '1.log')
+        output_file = output_file.replace('.log', '_1.log')
     if output_file:
         logger.add(output_file, enqueue=True, format=log_format)
     return logger
